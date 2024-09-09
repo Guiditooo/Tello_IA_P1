5c1c0cfd63e72330f244edf15fe8059a75b15356
Guido Tello <lpmaleinn@gmail.com>
2024-09-09 02:59:18 -0300

    Add Voronoi Class
    It will be the algorithm that will find the most "interesting" mine for the citizen.
    NOTE: At this Stage, this only calculates the Distance between all the points and returns the minor one.

---
+++ Assets/Parcial/Scripts/Voronoi.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FlyEngine;
+
+public class Voronoi
+{
+
+    private List<Vector2> nodeList; //Reemplazar por node para hacerlo por pesos
+
+    public Voronoi()
+    {
+        nodeList = new List<Vector2>();
+    }
+
+    public void AddNode(Vector2 v)
+    { 
+        nodeList.Add(v); 
+    }
+
+    public void AddNodes(List<Vector2> nodes)
+    {
+        nodeList.AddRange(nodes);
+    }
+
+    public Vector2 GetMostNearbyNode(Vector2 node) //Reemplazar por voronoi
+    {
+        if (nodeList == null || nodeList.Count == 0)
+            throw new ArgumentException("La lista de nodeList no puede estar vacía.");
+
+        Vector2 nearby = nodeList.OrderBy(n => Vector2.Distance(n, node)).First();
+
+        return nearby;
+    }
+
+
+}
