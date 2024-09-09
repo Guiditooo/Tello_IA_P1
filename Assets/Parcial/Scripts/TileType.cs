8e169e1b2b3db932e1cc623aa32214fef9e716e6
Guido Tello <lpmaleinn@gmail.com>
2024-09-09 02:50:42 -0300

    Add Tile & TileType
    In order to build the map, i have to do that.

---
+++ Assets/Parcial/Scripts/TileType.cs
@@ -0,0 +1,10 @@
+namespace FlyEngine
+{
+    public enum TileType
+    {
+        Dirt,      // 3 para Citizen - 3 para Caravana
+        Road,      // 2 para Citizen - 1 para Caravana
+        Mountain,  // 10 para Citizen - Imposible para Caravana
+        Stream     // Imposible para Citizen - 5 para Caravana
+    }
+}
